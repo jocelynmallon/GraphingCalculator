74dabccd60c6c30891865ce7b15f250f4cdb0048
Jocelyn Mallon <jocelyn.e.mallon@gmail.com>
2019-06-02 13:55:00 -0700

    Add icon to main stage/app

--- src/main/java/dev/StylishNerds/GraphingCalculator/Calculator.java
+++ src/main/java/dev/StylishNerds/GraphingCalculator/Calculator.java
@@ -8,6 +8,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -22,6 +23,8 @@ public class Calculator extends Application {
         stage.setMinHeight(510);
         stage.setResizable(true);
         stage.initStyle(StageStyle.UNIFIED);
+        stage.setTitle("Calculator");
+        stage.getIcons().add(new Image(getClass().getResource("/Calculator-icon.png").toExternalForm()));
         stage.show();
     }
 
