becd939bc155486cd1a592828456e3d9117036e8
Jocelyn Mallon <jocelyn.e.mallon@gmail.com>
2019-06-02 22:26:47 -0700

    update module-info.java
    Removed unused javafx.web, and added explicit requires of javafx.controls

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module GraphingCalculator {
     requires javafx.base;
     requires javafx.fxml;
     requires javafx.graphics;
-    requires javafx.web;
+    requires javafx.controls;
 
     exports dev.StylishNerds.GraphingCalculator;
     opens dev.StylishNerds.GraphingCalculator to javafx.fxml;
